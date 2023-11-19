30cb626a589b3d1fb4396db9b7c7554fa0fad150
Gordon Plumb <gplumb98@gmail.com>
2023-11-19 03:21:53 -0500

    use tags in list page, make tags smaller

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -1,3 +1,6 @@
+"use client"
+import { TagList } from "./TagList"
+
 type ListItemProps = {
   id: string,
   title: string,
@@ -9,7 +12,9 @@ export function ListItem({id, title, tags, runtime, watched}: ListItemProps) {
   return <li>
     <div className="flex gap-1">
       <label htmlFor={id} className="w-1/3">{title}</label>
-      <div className="flex gap-1 w-1/3">{tags.map(tag => (<p key={tag}>{tag}</p>))}</div>
+      <div className="flex gap-1 w-1/3">
+        <TagList listItemId={id} tags={tags} canEdit/>
+      </div>
       <p className="w-1/6">{runtime}</p>
       <input id={id} type="checkbox" className="w-1/6 cursor-pointer" checked={watched}/>
     </div>
